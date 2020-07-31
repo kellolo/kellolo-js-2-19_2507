39814333a7035042758d3b89512b7872c8eb84fd
Andrey Slipchenko <Kehey2011@gmail.com>
2020-07-31 22:16:06 +0300

    Lesson 2
    Почистил 0_max, и собрал его полностью, проверил, все работает
    Собрал полностью 0_mIn, но пришлось разделить файл main.js на cart.js и catalog,js , потому что при попытке собрать проект Webpack жаловался на то что я импортирую 2 вещи из одного файла, писал что я дублирую export default, так и не понял почему, но решил что поможет разделение файлов, и все собралось

---
+++ ST/Andrey Slipchenko/project/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    // entry: {
+
+    // },
+    // output: {
+
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: '[id],css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/imgs', to: 'img'}
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+        
+    ]
+}
