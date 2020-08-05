27be2d2ebccaba90242496b040165767726b2e1e
rezlers <rezlers@mail.ru>
2020-08-05 20:16:09 +0700

    Feat: completed task

--- ST/alex_rezler/project/src/components/js/GoodsList.js
+++ ST/alex_rezler/project/src/components/js/GoodsList.js
@@ -1,23 +1,28 @@
 import {GoodsItem} from './GoodsItem.js'
+import {makeGETRequest} from './helpers.js'
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 export class GoodsList {
     constructor() {
         this.goods = [];
     }
 
-    fetchGoods() {
-        this.goods = [
-            {title: 'Shirt', price: 150},
-            {title: 'Socks', price: 50},
-            {title: 'Jacket', price: 350},
-            {title: 'Shoes', price: 250},
-        ];
+    async fetchGoods() {
+        return new Promise((resolve, reject) => {
+            resolve();
+        }).then(() => {
+            return makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+                this.goods = JSON.parse(goods);
+                console.log(1)
+            })
+        })
     }
 
     calculatePrice() {
         let sumPrice = 0;
         this.goods.forEach((item) => {
-            sumPrice +=item.price
+            sumPrice += item.price
         })
         return sumPrice;
     }
@@ -25,11 +30,11 @@ export class GoodsList {
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
+            const goodItem = new GoodsItem(good.product_name, good.price);
             listHtml += goodItem.render();
         });
         const sumPriceDiv = document.createElement('div');
-        sumPriceDiv.setAttribute('class', 'summary-price')
+        sumPriceDiv.setAttribute('class', 'summary-price');
         sumPriceDiv.innerText = `Summary price is ${this.calculatePrice()}`;
         document.querySelector('header').appendChild(sumPriceDiv);
         document.querySelector('.goods-list').innerHTML = listHtml;
--- ST/alex_rezler/project/src/components/js/index.js
+++ ST/alex_rezler/project/src/components/js/index.js
@@ -3,6 +3,8 @@ import {GoodsItem} from './GoodsItem.js'
 
 export default () => {
     const list = new GoodsList();
-    list.fetchGoods();
-    list.render();
+    list.fetchGoods().then(() => {
+        console.log(2)
+        list.render();
+    })
 }
