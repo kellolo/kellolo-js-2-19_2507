cccd298fa5f52ee5f539a9646e1f524ea4c3e90d
SmolnyakovAS <a.s.smolnyakov@gmail.com>
2020-08-11 16:12:33 +0300

    4

--- ST/Smolnyakov Anton/project_max/src/components/index.js
+++ ST/Smolnyakov Anton/project_max/src/components/index.js
@@ -1,10 +1,15 @@
-import cart from "./js/cart";
-import catalog from "./js/catalog";
+import { Catalog, Cart } from './js/lists';
 import header from "./js/header"
 
 export default () => {
-    catalog(cart);
+    let catalog = new Catalog('/catalog.json', '#catalog');
+    let cart = new Cart('/cart.json', '#cart');
     header.handleEvents();
 }
 
+//{
+//  "amount": 150,
+//  "countGoods": 2,
+//  "contents":
+//}
 
