a3c96e7bf1cd09465864986a8120c9c64ef2604d
ivan kov <63105949+IKolyas@users.noreply.github.com>
2020-08-18 21:42:09 +0500

    Vue

--- ST/IvanKolyasnikov/project/src/components/index.js
+++ ST/IvanKolyasnikov/project/src/components/index.js
@@ -1,8 +1,10 @@
-import { Catalog, Basket } from "./js/lists";
+// import { Catalog, Basket, sumBasket } from "./js/lists";
+import {appBasket, appCatalog} from "./js/app";
 
 export default () => {
-    let basket = new Basket('/getBasket.json', '#basket' );
-    basket.openCart()
-    let catalog = new Catalog('/catalogData.json', '#index_catalog', basket);
-
+    // let basket = new Basket('/getBasket.json', '#basket' );
+    // basket.openCart()
+    // let catalog = new Catalog('/catalogData.json', '#index_catalog', basket);
+    let basket = appBasket;
+    let catalog = appCatalog;
 }
--- ST/IvanKolyasnikov/project/src/index.js
+++ ST/IvanKolyasnikov/project/src/index.js
@@ -1,6 +1,9 @@
-import app from "./components/index";
+// import app from "./components/index";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/dropdown';
 import './layout/styles/styles.scss';
 
+import app from './components/index'
+
 app()
+
