e0a38e4a0dbcd635ce8a389d82d0ee77df7f8a07
ivan kov <63105949+IKolyas@users.noreply.github.com>
2020-08-18 21:49:07 +0500

    vue

--- ST/IvanKolyasnikov/project/src/components/index.js
+++ ST/IvanKolyasnikov/project/src/components/index.js
@@ -1,10 +1,6 @@
-// import { Catalog, Basket, sumBasket } from "./js/lists";
 import {appBasket, appCatalog} from "./js/app";
 
 export default () => {
-    // let basket = new Basket('/getBasket.json', '#basket' );
-    // basket.openCart()
-    // let catalog = new Catalog('/catalogData.json', '#index_catalog', basket);
     let basket = appBasket;
     let catalog = appCatalog;
 }
