8e306e9f8dccb3ecbb7489724f2845979a912c89
Katrin Taltseva <talzeva@yandex.ru>
2020-08-19 14:32:10 +0400

    js2 dz4 (v3)

--- ST/Taltseva-Ekaterina/project/src/components/index.js
+++ ST/Taltseva-Ekaterina/project/src/components/index.js
@@ -1,15 +1,7 @@
 import { Catalog, Basket } from './js/lists';
 
 export default () => {
-    let basket = new Basket('/addToBasket.json', '#basket');
-    let catalog = new Catalog('/indexCatalogData.json', '#catalog', basket);
+    let basket = new Basket('/basket.json', '#basket');
+    let catalog = new Catalog('/catalog.json', '#catalog', basket);
 }
 
-
-// import basket from './js/basket';
-// import catalog from './js/catalog';
-
-// export default () => {
-//     basket.init();
-//     catalog.init(9);
-// }
--- ST/Taltseva-Ekaterina/project/src/components/js/lists.js
+++ ST/Taltseva-Ekaterina/project/src/components/js/lists.js
@@ -1,69 +1,74 @@
 import List from './list';
 
 export class Catalog extends List {
-    constructor(url, container, basket) {
-        super(url, container, basket);
-        this.basket = basket;
-    }
-    _handleActions() {
-        let cat = document.querySelector(this.container);
-        cat.addEventListener('click', evt => {
-          if (evt.target.name == 'add') {
-            let item = {
-              name: evt.target.dataset.name,
-              price: +evt.target.dataset.price,
-              img: evt.target.dataset.img,
-              amount: 1,
-              id: evt.target.dataset.id
-            }
-            this.basket.add(item);
-          }
-        });
-        document.addEventListener('click', evt => {
-          let main = document.querySelector('#cart');
-          if (evt.target.id == 'hide') {
-            main.classList.toggle('hidden');
-          }
-        })
-     }
-
+	constructor(url, container, basket) {
+		super(url, container, basket);
+		this.basket = basket;
+	}
+	_handleActions() {
+		let cat = document.querySelector(this.container);
+		cat.addEventListener('click', evt => {
+			if (evt.target.name == 'add') {
+				let item = {
+					name: evt.target.dataset.name,
+					price: +evt.target.dataset.price,
+					img: evt.target.dataset.img,
+					amount: 1,
+					id: evt.target.dataset.id
+				}
+				this.basket.add(item);
+			}
+		});
+		document.addEventListener('click', evt => {
+			let main = document.querySelector('#cart');
+			if (evt.target.id == 'hide') {
+				main.classList.toggle('hidden');
+			}
+		})
+	}
 }
 
 export class Basket extends List {
-    constructor(url, container) {
-        super(url, container);
-    }
-    _handleActions() {
-      let bas = document.querySelector(this.container);
-        bas.addEventListener('click', evt => {
-            if (evt.target.classList.contains('remove')) {
-                this.remove(evt.target.dataset.id);
-            }
-        })
-    }
-    add(item) {
-        let find = this.items.find(el => el.id == item.id);
+	constructor(url, container) {
+		super(url, container);
+		this._summ();
 
-        if (!find) {
-            this.items.push(item);
-            this.total = this.total + Number(item.price);
-        } else {
-            find.amount++;
-            this.total = this.total + Number(find.price);
-        }
-        this._render();
-    }
+	}
+	_handleActions() {
+		let cat = document.querySelector(this.container);
+		cat.addEventListener('click', evt => {
+			if (evt.target.name == 'remove') {
+				this.remove(evt.target.dataset.id);
+			}
+		})
+	}
+	add(item) {
+		let find = this.items.find(el => el.id == item.id);
+		if (!find) {
+			this.items.push(item);
+		} else {
+			find.amount++;
+		}
+		this.sum += item.price;
+		this._render();
+		[...document.querySelectorAll(".sum")].forEach(el => el.innerHTML = this.sum);
+	}
+	remove(itemId) {
+		let find = this.items.find(el => el.id == itemId);
 
-    remove(itemId) {
-        let find = this.items.find(el => el.id == itemId);
-
-        if (find.amount > 1) {
-            find.amount--;
-            this.total = this.total - Number(find.price);
-        } else {
-            this.items.splice(this.items.indexOf(find), 1);
-            this.total = this.total - Number(find.price);
-        }
-        this._render();
-    }
+		if (find.amount > 1) {
+			find.amount--;
+		} else {
+			this.items.splice(this.items.indexOf(find), 1);
+		}
+		this.sum -= find.price;
+		this._render();
+		[...document.querySelectorAll(".sum")].forEach(el => el.innerHTML = this.sum);
+	}
+	_summ() {
+    this._get('https://raw.githubusercontent.com/EkaterinaTaltseva/static/master/api/basket.json')
+			.then(data => {this.sum = data.sum})
+			.then(() => {[...document.querySelectorAll(".sum")].forEach(el => el.innerHTML = this.sum);
+			})
+	}
 }
