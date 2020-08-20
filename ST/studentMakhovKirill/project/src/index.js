7ca8bb18c4cb4417868c71d339d41ad1c7ad6eb5
Kirill <kir3811@mail.ru>
2020-08-20 22:39:40 +0300

    done

--- ST/studentMakhovKirill/project/src/index.js
+++ ST/studentMakhovKirill/project/src/index.js
@@ -5,72 +5,79 @@ import './layout/styles/style.css';
 
 // app();
 
-let app =  new Vue({
-	el: '#app',
-	data: {
-		//здесь все "переменные", которые связаны с реактивным поведением (перерендером)
-		catalogUrl: 'https://raw.githubusercontent.com/K-Real-Mh/Static/master/JSON/catalog.json',
-		cartUrl: 'https://raw.githubusercontent.com/K-Real-Mh/Static/master/JSON/getBasket.json',
-		catalogItems: [],
-		cartItems: [],
-		cartShown: true,
-		searchText: '',
-		sum: ''
-	},
-	methods: {
-		get(url) {
-			return fetch(url).then(d => d.json())
-		},
-		add(item) {
-			let cartItem = {
-					name: item.name,
-					price: +item.price,
-					img: item.img,
-					amount: 1,
-					id: item.id
-			}
-			let find = this.cartItems.find(el => el.id == cartItem.id);
-			if (!find) {
-				this.cartItems.push(cartItem);
-			} else {
-				find.amount++;
-			}
-			this.sum += cartItem.price;
-		},
-		remove(item) {
-			let find = this.cartItems.find(el => el.id == item.id);
+import Vue from 'vue';
+import app from './views/main.vue';
+
+new Vue({
+	render: h => h(app)
+}).$mount('#app');
+
+// let app =  new Vue({
+// 	el: '#app',
+// 	data: {
+// 		//здесь все "переменные", которые связаны с реактивным поведением (перерендером)
+// 		catalogUrl: 'https://raw.githubusercontent.com/K-Real-Mh/Static/master/JSON/catalog.json',
+// 		cartUrl: 'https://raw.githubusercontent.com/K-Real-Mh/Static/master/JSON/getBasket.json',
+// 		catalogItems: [],
+// 		cartItems: [],
+// 		cartShown: true,
+// 		searchText: '',
+// 		sum: ''
+// 	},
+// 	methods: {
+// 		get(url) {
+// 			return fetch(url).then(d => d.json())
+// 		},
+// 		add(item) {
+// 			let cartItem = {
+// 					name: item.name,
+// 					price: +item.price,
+// 					img: item.img,
+// 					amount: 1,
+// 					id: item.id
+// 			}
+// 			let find = this.cartItems.find(el => el.id == cartItem.id);
+// 			if (!find) {
+// 				this.cartItems.push(cartItem);
+// 			} else {
+// 				find.amount++;
+// 			}
+// 			this.sum += cartItem.price;
+// 		},
+// 		remove(item) {
+// 			let find = this.cartItems.find(el => el.id == item.id);
 	
-			if (find.amount > 1) {
-				find.amount--;
-			} else {
-				this.cartItems.splice(this.cartItems.indexOf(find), 1);
-			}
-			this.sum -= find.price;
-		}
-		// filter() {
-		// 	let reg = new RegExp(this.searchText, 'i');
-		// 	this.catalogItemsFiltered = this.catalogItems.filter(item => item.productName.match(reg));
-		// }
-	},
-	computed: {
-		//вычисляемые значения
-		buttonPlaceholder() {
-			return this.cartShown ? 'Hide' : 'Show'
-		}
-	},
-	// watch: {
-	//     //следящие
-	// },
-	//хуки жизненного цикла
-	mounted() {
-		this.get(this.catalogUrl).then(items => {
-			this.catalogItems = items;
-			//this.catalogItemsFiltered = items;
-		}),
-		this.get(this.cartUrl).then(items => {
-			this.cartItems = items.content;
-			this.sum = items.sum;
-			//this.catalogItemsFiltered = items;
-		})
-	}
-});
+// 			if (find.amount > 1) {
+// 				find.amount--;
+// 			} else {
+// 				this.cartItems.splice(this.cartItems.indexOf(find), 1);
+// 			}
+// 			this.sum -= find.price;
+// 		}
+// 		// filter() {
+// 		// 	let reg = new RegExp(this.searchText, 'i');
+// 		// 	this.catalogItemsFiltered = this.catalogItems.filter(item => item.productName.match(reg));
+// 		// }
+// 	},
+// 	computed: {
+// 		//вычисляемые значения
+// 		buttonPlaceholder() {
+// 			return this.cartShown ? 'Hide' : 'Show'
+// 		}
+// 	},
+// 	// watch: {
+// 	//     //следящие
+// 	// },
+// 	//хуки жизненного цикла
+// 	mounted() {
+// 		this.get(this.catalogUrl).then(items => {
+// 			this.catalogItems = items;
+// 			//this.catalogItemsFiltered = items;
+// 		}),
+// 		this.get(this.cartUrl).then(items => {
+// 			this.cartItems = items.content;
+// 			this.sum = items.sum;
+// 			//this.catalogItemsFiltered = items;
+// 		})
+// 	}
+// });
---
+++ ST/studentMakhovKirill/project/src/views/main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header_ />
+    <content_ />
+    <foot />
+    <copyright />
+  </div>
+</template>
+
+<script>
+import catalog from '../components/catalog.vue';
+import foot from '../components/footer.vue';
+import copyright from '../components/copyright.vue';
+import header_ from '../components/header.vue';
+import content_ from '../components/content.vue';
+export default {
+	components: {
+		catalog,
+		foot,
+		copyright,
+		header_,
+		content_
+	},
+	methods: {
+		parentGet(url) {
+			return fetch(url).then(d => d.json())
+		}
+	}
+};
+</script>
+
+<style>
+</style>
