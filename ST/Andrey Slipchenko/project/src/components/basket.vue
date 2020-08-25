f4dadfced35dc1edb1427f7d588d18a31ca5b305
Rutard-Hash <60180756+Rutard-Hash@users.noreply.github.com>
2020-08-25 06:53:28 +0300

    Lesson6

---
+++ ST/Andrey Slipchenko/project/src/components/basket.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="drop header__basket">
+        <div class="drop__flex">
+            <div id="basket">
+                <item
+                    v-for="item of items"
+                    type="basket"
+                    :item="item"
+                    :key="item.productId" 
+                />
+            </div>
+            <div class="basket__under">
+                <div class="basket__under_total">
+                    <h3 class="total__text">TOTAL</h3>
+                    <h3 class="total__price">&#36;500.00</h3>
+                </div>
+                <button class="basket__button_first">checkout</button>
+                <button class="basket__button_second">go to cart</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import item from './item.vue';
+
+export default {
+    components: { item },
+    data() {
+        return {
+            url: 'https://raw.githubusercontent.com/Rutard-Hash/static/master/JSON/basket.json',
+            items: []
+        }
+    },
+    mounted() {
+        this.$parent.parentGet(this.url)
+        .then(d => { this.items = d.content })
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ ST/Andrey Slipchenko/project/src/components/item.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <template v-if="type == 'catalog'">
+            <div class="product__item">
+                <a href="single page.html"><img class="product__image" :src="item.productImg"></a>
+                <div class="product__description">
+                    <a class="product__name" href="single page.html">{{item.productName}}</a>
+                    <div class="product__price">&#36; {{item.productPrice}}</div>
+                </div>
+                <a class="product__add" href="#">Add to Cart</a>
+            </div>
+        </template>
+
+
+        <template v-if="type == 'basket'">
+            <div class="basket__item">
+                <div class="basket__item_img"><img :src="item.productImg" alt="basket-item-img"></div>
+                <div class="basket__item_desc">
+                    <h4 class="basket__item_name">{{item.productName}}</h4>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                    <div class="calc">
+                        <h5 class="amount"> {{ item.amount }} x</h5>
+                        <h5 class="price">&nbsp;&#36; {{ item.productPrice }}</h5>
+                    </div>
+                </div>
+                <button class="basket__item_icon" id="remove">
+                    <i class="fas fa-times-circle"></i>
+                </button>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'catalog'
+        },
+        item: { type: Object }
+    }
+}
+</script>
+
+<style>
+
+</style>
